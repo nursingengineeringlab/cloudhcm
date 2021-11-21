32137d4c01c24915aae42c1d4bb1ee88626ac397
shiywang <shiyangwang@umass.edu>
2021-11-21 17:34:03 -0500

    Revert "rename folder"

--- server/api/care_api/data_api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from data_api.models import Senior, FamilyMember, CareProvider, Sensor_Data
-
-# Register your models here.
-admin.site.register(Senior)
-admin.site.register(FamilyMember)
